32e27411179cf321dfa0542c969167727e333a6d
Janne Huovari <janne.huovari@ptt.fi>
2015-09-25 13:03:43 +0300

    M and Q dates for clean_times

--- R/times.R
+++ R/times.R
@@ -11,16 +11,18 @@
 #' @examples
 #' public_debt2 <- clean_times(public_debt)
 #' str(public_debt2)
+#' str(clean_times(output_ind, time_format = "date"))
 
 
 clean_times <- function(x, time_format = NULL,
                         year_col = "Vuosi", sub_year_col = NULL){
   # guess sub year
   if (is.null(sub_year_col)) {
-    sub_years <- c("Neljännesvuosi", "Neljannesvuosi", "Kuukausi")
-    sub_years[sub_years %in% names(x)]
+    sub_years <- c("Nelj\u00e4nnesvuosi", "Neljannesvuosi", "Kuukausi")
+    sub_year_col <- sub_years[sub_years %in% names(x)]
   }
 
+  # Only yearly
   if (length(sub_year_col) == 0){
     if (is.null(time_format) || time_format == "num"){
       x$time <- tidyr::extract_numeric(x[, year_col])
@@ -30,8 +32,28 @@ clean_times <- function(x, time_format = NULL,
     }
     x[, year_col] <- NULL
     return(x)
+
+  # mounthly or quertely
+  } else {
+
+    subs <- as.character(unique(x[[sub_year_col]]))
+
+    # TODO tarkistus subs == ensimmäiset
+
+    mounths <- seq.int(1, by = 12/length(subs), length.out = length(subs))
+
+    if (is.null(time_format) || time_format == "num"){
+      x$time <- tidyr::extract_numeric(x[, year_col]) +
+        (mounths - 1) * 1/length(mounths)
+    } else {
+      x$time <- as.Date(
+        paste0(tidyr::extract_numeric(x[, year_col]), "-",
+               mounths[match(subs, x[[sub_year_col]])] , "-1"))
+    }
+    x[, year_col] <- NULL
+    x[, sub_year_col] <- NULL
+    return(x)
   }
 
-  stop("TODO")
 
 }
--- data-raw/get_raw_data.R
+++ data-raw/get_raw_data.R
@@ -8,3 +8,14 @@ public_debt <- pxweb::get_pxweb_data(url = "http://pxnet2.stat.fi/PXWeb/api/v1/f
                                     clean = TRUE)
 
 devtools::use_data(public_debt, overwrite = TRUE)
+
+output_ind <-
+  get_pxweb_data(url = "http://pxnet2.stat.fi/PXWeb/api/v1/fi/StatFin/kan/ktkk/110_ktkk_tau_101.px",
+                 dims = list(Toimiala = c('0'),
+                             Tiedot = c('IND100'),
+                             Sarjatyyppi = c('*'),
+                             Vuosi = c('*'),
+                             Kuukausi = c('*')),
+                 clean = TRUE)
+
+devtools::use_data(output_ind, overwrite = TRUE)
